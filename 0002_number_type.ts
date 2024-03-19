f58f3b4ecb8643bdbf59271f9551dfc78434c091
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-20 00:39:57 +0300

    Create 0002_number_type.ts

---
+++ 0002_number_type.ts
@@ -0,0 +1,19 @@
+//index.ts file
+
+let year: number = 2024
+
+let exchangeRate: number = 32.21
+
+let hisSalary: number = 5250
+
+console.log(`This year is ${year}.`)
+
+console.log(`Current exchange rate is ${exchangeRate}!`)
+
+console.log(`Is his salary amazing? \$${hisSalary}...`)
+
+/*
+This year is 2024.
+Current exchange rate is 32.21!      
+Is his salary amazing? $5250...   
+*/
