63ecb47d8da31ec7bcfc341b48b2053cf31a301e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-20 21:22:55 +0300

    Create 0005_arrays1.ts

---
+++ 0005_arrays1.ts
@@ -0,0 +1,11 @@
+//index.ts file
+
+let areas: string[] = ["DeFi", "Web3", "Blockchain"]
+
+let names: Array<string> = ["Mustafa", "Kağan", "Merve"]
+
+console.log(areas[1]) // Web3
+
+let info = ["Hakan", 29, true]
+
+console.log(`Name: ${info[0]}, age: ${info[1]} and married: ${info[2]}`) //Name: Hakan, age: 29 and married: true
