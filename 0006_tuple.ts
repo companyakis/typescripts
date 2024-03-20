66cbb19531a26330aefeefd9e5363cb4c2c91e02
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-20 21:48:16 +0300

    Create 0006_tuple.ts

---
+++ 0006_tuple.ts
@@ -0,0 +1,9 @@
+//index.ts file
+
+let whoIsReady1: [string , boolean] = ["Mustafa", true]
+
+let whoIsReady2: [string , boolean] = ["Aygün", false]
+
+let completeInfo: [string, number, number, boolean]
+
+completeInfo = ["Arslan", 21, 14000, false]
