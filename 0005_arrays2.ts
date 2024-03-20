9c9696391aa9b81b079346364e3fb1fd252c3853
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-20 21:25:48 +0300

    Create 0005_arrays2.ts

---
+++ 0005_arrays2.ts
@@ -0,0 +1,17 @@
+//index.ts file
+
+let ages: Array<number>
+
+ages = [21, 32, 76, 97]
+
+for (let i = 0; i < ages.length; i++) {
+    console.log("Age: " + ages[i])
+}
+
+/*
+Age: 21
+Age: 32
+Age: 76
+Age: 97
+
+*/
