2064e5df92333c2163432fa36903080f05d4943e
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-21 23:57:39 +0300

    Create 0008_enum2.ts

---
+++ 0008_enum2.ts
@@ -0,0 +1,11 @@
+enum PersonFeelings {
+    Mustafa = "A rolling stone gathers no moss",
+    Aygun = "Everything comes to him who waits",
+    Kagan = "Out of sight, out of mind",
+}
+
+console.log(PersonFeelings.Mustafa) //A rolling stone gathers no moss
+
+console.log(PersonFeelings["Aygun"]) //Everything comes to him who waits
+    
+
