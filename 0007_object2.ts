3561d155f41994c598236908e601626278677670
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-21 23:38:01 +0300

    Create 0007_object2.ts

---
+++ 0007_object2.ts
@@ -0,0 +1,30 @@
+let mustafa : {
+    firstName: string,
+    lastName: string,
+    id: number,
+    startingYear: number,
+    department: string,
+}
+
+mustafa = {
+    firstName : "Mustafa",
+    lastName : "Buyukdereli",
+    id : 1,
+    startingYear : 2024,
+    department : "Boarding Member"
+}
+
+console.log(mustafa)
+
+console.log(mustafa.lastName)
+
+/*
+{
+  firstName: 'Mustafa',
+  lastName: 'Buyukdereli',
+  id: 1,
+  startingYear: 2024,
+  department: 'Boarding Member'
+}
+Buyukdereli
+*/
