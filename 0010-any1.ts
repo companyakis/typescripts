e1c1248911d86aca51d1ef01bbd74de9d6618a69
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-02 23:37:13 +0300

    Create 0010-any1.ts

---
+++ 0010-any1.ts
@@ -0,0 +1,14 @@
+//index.ts file
+
+let someInfo: any = "Mustafa"
+
+someInfo = 2024
+
+someInfo = false
+
+someInfo = {
+    languageOne: "TypeScript",
+    languageTwı: "Rust",
+}
+
+console.log(someInfo) //{ languageOne: 'TypeScript', 'languageTwı': 'Rust' }
