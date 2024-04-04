c58ce5c4d1e090a06b17a8faee37c69c4f698e7d
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-04 22:20:51 +0300

    Create 0015-conditional.ts

---
+++ 0015-conditional.ts
@@ -0,0 +1,11 @@
+let a = 30
+
+let b = 41
+
+if (a > b || a == 30 ) {
+    console.log("a > b or a is equal to 30.")
+} else if (b > a) {
+    console.log("Yes, b > a!")
+} else {
+    console.log("Not important!")
+}
