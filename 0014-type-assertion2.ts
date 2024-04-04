2ccb25bb40ebddbcc916e8790ecb9f8128e08ea0
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-04 22:10:08 +0300

    Create 0014-type-assertion2.ts

---
+++ 0014-type-assertion2.ts
@@ -0,0 +1,13 @@
+interface Student {
+    name: string,
+    class: string,
+    classNumber: number
+}
+
+let ceyhun = <Student> {}
+
+ceyhun.name = "Ceyhun"
+
+ceyhun.class = "10 E"
+
+ceyhun.classNumber = 440
