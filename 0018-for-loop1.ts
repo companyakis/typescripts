fc11cf94dfc700832a2bf8225d2691ba5819882f
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-04 23:43:49 +0300

    Create 0018-for-loop1.ts

---
+++ 0018-for-loop1.ts
@@ -0,0 +1,16 @@
+for (let i = 0; i < 10; i++) {
+    console.log(`Number_${i + 1}: ${i}`)
+}
+
+/*
+Number_1: 0
+Number_2: 1
+Number_3: 2
+Number_4: 3
+Number_5: 4
+Number_6: 5
+Number_7: 6
+Number_8: 7
+Number_9: 8
+Number_10: 9
+*/
