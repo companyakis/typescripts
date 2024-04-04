91416a413ce5d35f116b8f8267cdedca7976270a
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-04 23:51:30 +0300

    Create 0018-for-loop3.ts

---
+++ 0018-for-loop3.ts
@@ -0,0 +1,32 @@
+let me = "Mustafa Büyükdereli"
+
+let c = ""
+
+for (let char of me) {
+    c += char + " "
+    console.log(c)
+}
+
+/*
+
+M 
+M u     
+M u s   
+M u s t 
+M u s t a
+M u s t a f
+M u s t a f a
+M u s t a f a
+M u s t a f a   B
+M u s t a f a   B ü
+M u s t a f a   B ü y
+M u s t a f a   B ü y ü
+M u s t a f a   B ü y ü k
+M u s t a f a   B ü y ü k d
+M u s t a f a   B ü y ü k d e
+M u s t a f a   B ü y ü k d e r
+M u s t a f a   B ü y ü k d e r e
+M u s t a f a   B ü y ü k d e r e l
+M u s t a f a   B ü y ü k d e r e l i
+
+*/
