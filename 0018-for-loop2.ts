8a83bed953093c4b53b1409741c1889c31c64434
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-04 23:46:38 +0300

    Create 0018-for-loop2.ts

---
+++ 0018-for-loop2.ts
@@ -0,0 +1,14 @@
+let arr: number[] = [100, 200, 300, -5000]
+
+for (let item of arr) {
+    console.log("List item: ", item)
+}
+
+/*
+
+List item:  100
+List item:  200
+List item:  300
+List item:  -5000
+
+*/
