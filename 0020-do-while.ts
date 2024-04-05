d88dcd897dd669d469eed812b12eb00cbe4c7900
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-04-05 14:56:07 +0300

    Create 0020-do-while.ts

---
+++ 0020-do-while.ts
@@ -0,0 +1,19 @@
+let counter = 12
+
+do {
+
+    counter--
+
+    console.log("Counter value: ", counter)
+
+} while (counter > 5)
+
+/*
+Counter value:  11
+Counter value:  10
+Counter value:  9
+Counter value:  8
+Counter value:  7
+Counter value:  6
+Counter value:  5
+*/
